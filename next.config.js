f31da475395fa2d7895356ce20f8a1eb931d2629
suppg022312 <suppg02@gmail.com>
2025-02-27 22:56:22 +0000

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
- 
+
 module.exports = {
+  output: 'export',
+  basePath: '/developer-portfolio',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
